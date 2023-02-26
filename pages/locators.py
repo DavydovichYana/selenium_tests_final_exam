d9081fbfc37846a810b0e0e6fcde4433dc3c5c49
DavydovichYana <luckyabsolut@gmail.com>
2023-02-26 12:52:38 +0300

    add LoginPage

---
+++ pages/locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "[name='login-username']")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "[name='login-password']")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "[name='registration-email']")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "[name='registration-password1']")
+    REGISTER_PASSWORD_REPEAT = (By.CSS_SELECTOR, "[name='registration-password2']")
